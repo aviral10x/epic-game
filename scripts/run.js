eea379694d74d7cdcab0f52ada5a1e61b50e4f4a
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-09-01 16:31:38 +0530

    4

--- scripts/run.js
+++ scripts/run.js
@@ -11,8 +11,19 @@ const main = async () => {
     [100, 50, 25] // Attack damage values
   );
   await gameContract.deployed();
-  console.log("Contract deployed to:", gameContract.address);
+    console.log("Contract deployed to:", gameContract.address);
+    let txn;
+    // We only have three characters.
+    // an NFT w/ the character at index 2 of our array.
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+
+    // Get the value of the NFT's URI.
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log("Token URI:", returnedTokenUri);
 };
+
+
 
 const runMain = async () => {
   try {
